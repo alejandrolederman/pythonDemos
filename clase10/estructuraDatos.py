31127d3b058ed7c03b2dc40f22c048e7d48a7dfe
alejandrolederman <alejandro.lederman@live.com.ar>
2022-06-30 21:23:57 -0300

    actual

---
+++ clase10/estructuraDatos.py
@@ -0,0 +1,5 @@
+lista_1 = list()
+print(lista_1)
+
+lista_2 =  list((2,3,10))
+print(lista_2)
---
+++ clase10/funcionesEntrada.py
@@ -0,0 +1,9 @@
+# mensjae = input("ingrese u mensaje: ")
+notas = open ("./clase10/nota.txt")
+
+print  (type (notas))
+
+print (notas)
+
+for linea in notas:
+    print(linea)
